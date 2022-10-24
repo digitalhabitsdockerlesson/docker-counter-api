ea1897a09a1e37d0d2cec24621b4d8da0b6e6f3b
Rostislav Blagushin <blagrost11@gmail.com>
2022-10-24 21:00:16 +0300

    Fix hello world endpoint

--- src/main/java/ru/digitalhabbits/docker/controller/CounterController.java
+++ src/main/java/ru/digitalhabbits/docker/controller/CounterController.java
@@ -32,6 +32,6 @@ public class CounterController {
 
     @GetMapping("/test")
     public ResponseEntity<String> getHelloWorld() {
-        return ResponseEntity.ok("Hello, World!");
+        return ResponseEntity.ok("Hello, digital habits!");
     }
 }
