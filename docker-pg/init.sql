af8ab7812c715c94599a7c6b9adc5b9dd183b2e5
DiscoverSSV <discover.ssv@gmail.com>
2022-11-06 03:20:21 +0600

    Added: - Dockerfile for Postgres DB and init.sql - Docker-compose for deploy all services on local docker desktop Fixed: - github workflows gradle.yml deleted action push on cloud server

---
+++ docker-pg/init.sql
@@ -0,0 +1,13 @@
+CREATE USER counter WITH PASSWORD 'counter-pass';
+
+CREATE DATABASE counter;
+GRANT ALL PRIVILEGES ON DATABASE counter TO counter;
+
+\c counter;
+
+create table COUNTER (
+    ID bigserial primary key,
+    VALUE bigint not null default 0
+);
+
+insert into COUNTER(value) values (0);
